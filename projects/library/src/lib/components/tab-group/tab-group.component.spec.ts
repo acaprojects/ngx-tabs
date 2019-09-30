50492952987d3b1257e2b2a36601470d8303baf8
Alex Sorafumo <alex@yuion.net>
2019-09-30 13:20:17 +1000

    test(tabs): add tests for tab group and tab components

--- projects/library/src/lib/components/tab-group/tab-group.component.spec.ts
+++ projects/library/src/lib/components/tab-group/tab-group.component.spec.ts
@@ -1,25 +1,79 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { APipesModule } from '@acaprojects/ngx-pipes';
 
 import { TabGroupComponent } from './tab-group.component';
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+    selector: 'test-tab-group',
+    template: `<a-tab-group tab="group-3">
+        <a-tab id="group-1" [content]="html">Tab 1</a-tab>
+        <a-tab id="group-2" [content]="template">Tab 2</a-tab>
+        <a-tab id="group-3">Tab 3</a-tab>
+        <a-tab id="group-4">Tab 4</a-tab>
+        <a-tab id="group-5">Tab 5</a-tab>
+        <a-tab id="group-6">Tab 6</a-tab>
+        <a-tab id="group-7">Tab 7</a-tab>
+        <a-tab id="group-8">Tab with a really really long name</a-tab>
+        <a-tab id="group-9">Tab with a really really long name too</a-tab>
+    </a-tab-group>
+    <ng-template #template>
+        <div class="template">Template</div>
+    </ng-template>`
+})
+class TestWrapperComponent {
+    public html = `<div class="html"></div>`;
+}
 
 describe('TabGroupComponent', () => {
-  let component: TabGroupComponent;
-  let fixture: ComponentFixture<TabGroupComponent>;
+    let component: TabGroupComponent;
+    let fixture: ComponentFixture<TestWrapperComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TabGroupComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [TestWrapperComponent, TabGroupComponent, TabComponent],
+            imports: [CommonModule, APipesModule]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TabGroupComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TestWrapperComponent);
+        component = fixture.debugElement.children[0].componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create component', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should activate set tab on init', () => {
+        const compiled: HTMLElement = fixture.debugElement.children[0].nativeElement;
+        const tabs = compiled.querySelectorAll('.tab');
+        expect(tabs[2]).toBeTruthy();
+        const active_tab = compiled.querySelector('.tab.active');
+        expect(tabs[2]).toBe(active_tab);
+    });
+
+    it('should show tab headers', () => {
+        const compiled: HTMLElement = fixture.debugElement.children[0].nativeElement;
+        const first_tab = compiled.querySelector('.tab');
+        expect(first_tab).toBeTruthy();
+        expect(compiled.querySelectorAll('.tab').length).toBe(9);
+    });
+
+    it('should handle switching tabs', () => {
+        const compiled: HTMLElement = fixture.debugElement.children[0].nativeElement;
+        const tab_list = compiled.querySelectorAll('.tab');
+        expect(compiled.querySelector('.html')).toBeFalsy();
+        tab_list[0].dispatchEvent(new Event('mousedown'));
+        fixture.detectChanges();
+        expect(component.tab).toBe('group-1');
+        expect(compiled.querySelector('.html')).toBeTruthy();
+        tab_list[1].dispatchEvent(new Event('mousedown'));
+        fixture.detectChanges();
+        expect(component.tab).toBe('group-2');
+        expect(compiled.querySelector('.template')).toBeTruthy();
+    });
 });
--- projects/library/src/lib/components/tab/tab.component.spec.ts
+++ projects/library/src/lib/components/tab/tab.component.spec.ts
@@ -1,25 +1,62 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
 
 import { TabComponent } from './tab.component';
 
+@Component({
+    selector: 'test-tab-group',
+    template: `<a-tab id="group-1"><span>Tab 1</span></a-tab>`
+})
+class TestWrapperComponent {
+    public html = `<div class="html"></div>`;
+}
+
 describe('TabComponent', () => {
-  let component: TabComponent;
-  let fixture: ComponentFixture<TabComponent>;
+    let component: TabComponent;
+    let fixture: ComponentFixture<TestWrapperComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TabComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [TestWrapperComponent, TabComponent]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TabComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TestWrapperComponent);
+        component = fixture.debugElement.children[0].componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create component', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should show contents', () => {
+        const compiled: HTMLElement = fixture.debugElement.children[0].nativeElement;
+        const el = compiled.querySelector('.tab');
+        expect(el).toBeTruthy();
+        expect(el.innerHTML).toBe(`<span>Tab 1</span>`);
+    });
+
+    it('should activate on user interaction', () => {
+        const compiled: HTMLElement = fixture.debugElement.children[0].nativeElement;
+        const el = compiled.querySelector('.tab');
+        expect(component.active).toBeFalsy();
+        el.dispatchEvent(new Event('mousedown'));
+        expect(component.active).toBeTruthy();
+        component.active = false;
+        el.dispatchEvent(new Event('touchstart'));
+        expect(component.active).toBeTruthy();
+    });
+
+    it('should emit activations', (done) => {
+        const compiled: HTMLElement = fixture.debugElement.children[0].nativeElement;
+        const el = compiled.querySelector('.tab');
+        component.activeChange.subscribe((state) => {
+            expect(state).toBeTruthy();
+            done();
+        })
+        expect(component.active).toBeFalsy();
+        el.dispatchEvent(new Event('mousedown'));
+    });
 });
